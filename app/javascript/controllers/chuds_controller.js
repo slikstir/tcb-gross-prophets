673b6c447392bbde728ead51e7d50a151e6f8820
Ricky Winowiecki <slikstir@gmail.com>
2025-02-17 19:25:07 -0500

    chuds Vote UI

--- app/javascript/controllers/chuds_controller.js
+++ app/javascript/controllers/chuds_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input", "amount"]
+  static targets = ["input", "amount", "submit", "warning"]
   static values = { balance: Number }
 
 
@@ -10,8 +10,36 @@ export default class extends Controller {
   }
 
   updateTotal() {
+    let startingBalance = this.balanceValue // Get initial balance from data attribute
     let total = this.inputTargets.reduce((sum, input) => sum + (parseInt(input.value, 10) || 0), 0)
-    let newBalance = this.balanceValue - total
+    let newBalance = startingBalance - total
+
     this.amountTarget.textContent = newBalance < 0 ? 0 : newBalance // Prevent negative values
+
+    if (newBalance < 0) {
+      this.submitTarget.disabled = true // Disable submit
+      this.warningTarget.classList.remove("d-none") // Show warning
+    } else {
+      this.submitTarget.disabled = false // Enable submit
+      this.warningTarget.classList.add("d-none") // Hide warning
+    }
+  }
+
+  increment(event) {
+    event.preventDefault()
+    let input = event.currentTarget.closest("td").querySelector(".chuds-amount")
+    if (input) {
+      input.value = parseInt(input.value, 10) + 1
+      this.updateTotal()
+    }
+  }
+
+  decrement(event) {
+    event.preventDefault()
+    let input = event.currentTarget.closest("td").querySelector(".chuds-amount")
+    if (input && parseInt(input.value, 10) > 0) {
+      input.value = parseInt(input.value, 10) - 1
+      this.updateTotal()
+    }
   }
 }
