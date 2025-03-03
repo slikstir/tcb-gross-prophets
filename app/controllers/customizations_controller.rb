70430f6056b01bf49d77f8c33dde251dc243af16
Ricky Winowiecki <slikstir@gmail.com>
2025-03-03 12:40:04 -0500

    Attempt to fix CSS customizations

--- app/controllers/customizations_controller.rb
+++ app/controllers/customizations_controller.rb
@@ -1,12 +1,16 @@
 class CustomizationsController < ApplicationController
   def styles
-    setting = Setting.find_by(code: "custom_css")
+    setting = Setting.find_by(name: "custom_css")
     @custom_css = setting&.value || ""
 
-    # Set cache headers (optional, helps control browser caching)
-    expires_now # Forces cache expiration
+    # Explicitly set Content-Type to CSS
+    response.headers["Content-Type"] = "text/css; charset=utf-8"
+
+    # Prevent aggressive caching on iOS Safari
     response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
-    
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+
     respond_to do |format|
       format.css { render layout: false }
     end
