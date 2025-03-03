3c2ca5027ea2a7fbbd3c018920e148ed567e3699
Ricky Winowiecki <slikstir@gmail.com>
2025-03-03 13:12:33 -0500

    CSS As a separate file fixed.

--- app/controllers/customizations_controller.rb
+++ app/controllers/customizations_controller.rb
@@ -3,16 +3,6 @@ class CustomizationsController < ApplicationController
     setting = Setting.find_by(code: "custom_css")
     @custom_css = setting&.value || ""
 
-    # Explicitly set Content-Type to CSS
-    response.headers["Content-Type"] = "text/css; charset=utf-8"
-
-    # Prevent aggressive caching on iOS Safari
-    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
-
-    respond_to do |format|
-      format.css { render layout: false }
-    end
+    render body: @custom_css, content_type: "text/css"
   end
 end
