6f1e2c6570288da8f89f575781d8b97c0e614b72
Ricky Winowiecki <slikstir@gmail.com>
2025-03-04 12:30:19 -0500

    Fade in on broadcast from Turbo Stream

--- app/javascript/controllers/notifications_controller.js
+++ app/javascript/controllers/notifications_controller.js
@@ -2,13 +2,23 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
+    this.fadeIn();
     this.dismissAfterTimeout();
+  }
+
+  fadeIn() {
+    this.element.style.opacity = 0; // Start hidden
+    this.element.style.transition = "opacity 0.5s ease-in-out"; // Smooth transition
+
+    requestAnimationFrame(() => {
+      this.element.style.opacity = 1; // Fade in
+    });
   }
 
   dismissAfterTimeout() {
     setTimeout(() => {
-      this.element.classList.add("fade-out");
-      setTimeout(() => this.element.remove(), 500);
+      this.element.style.opacity = 0; // Start fade out
+      setTimeout(() => this.element.remove(), 500); // Remove after fade out
     }, 3000);
   }
 }
