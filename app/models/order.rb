5aa397b40288bf6f889a8ade04ab2a338531da09
Ricky Winowiecki <slikstir@gmail.com>
2025-03-09 20:07:39 -0400

    Paginating 100 Orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -31,6 +31,8 @@ require 'csv'
 include Rails.application.routes.url_helpers
 
 class Order < ApplicationRecord
+  paginates_per 100
+
   CURRENCIES =  %w[
             usd eur gbp jpy cny rmb aud cad chf hkd sgd
             nzd inr brl rub zar krw mxn idr try thb
