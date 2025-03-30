e610a56e8c63328eb82a9f928270e31f0601a8d3
Ricky Winowiecki <slikstir@gmail.com>
2025-03-30 13:05:56 +1100

    Testing Cloudfront for Product

--- config/routes.rb
+++ config/routes.rb
@@ -133,4 +133,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  direct :rails_public_blob do |blob|
+    # Preserve the behaviour of `rails_blob_url` inside these environments
+    # where S3 or the CDN might not be configured
+    if ENV.fetch("ACTIVE_STORAGE_ASSET_HOST", false) && blob&.key
+     File.join(ENV.fetch("ACTIVE_STORAGE_ASSET_HOST"), blob.key)
+    else
+     route =
+        # ActiveStorage::VariantWithRecord was introduced in Rails 6.1
+       # Remove the second check if you're using an older version
+       if blob.is_a?(ActiveStorage::Variant) || blob.is_a?(ActiveStorage::VariantWithRecord)
+          :rails_representation
+       else
+          :rails_blob
+       end
+     route_for(route, blob)
+    end
+  end
 end
