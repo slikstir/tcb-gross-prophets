273873c407084147eaeae84b827b8a8ca075a668
Ricky Winowiecki <slikstir@gmail.com>
2025-03-30 15:26:17 +1100

    In show carts only

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -13,7 +13,7 @@ class AttendeesController < ApplicationController
 
     session[:show_code] = params[:show_code].downcase
     session[:email] = @attendee.email
-    if @attendee.carts.any?
+    if @attendee.carts.in_show.any?
       session[:show_order_id] = @attendee.carts.in_show.last.id
     else
       session[:show_order_id] = nil
