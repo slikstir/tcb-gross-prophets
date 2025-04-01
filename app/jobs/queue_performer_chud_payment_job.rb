646433c7b6b4a870d17b20843b5e0fc70da2e719
Ricky Winowiecki <slikstir@gmail.com>
2025-04-01 13:38:35 +1100

    Fixed the lock

--- app/jobs/queue_performer_chud_payment_job.rb
+++ app/jobs/queue_performer_chud_payment_job.rb
@@ -2,7 +2,9 @@ class QueuePerformerChudPaymentJob < ApplicationJob
   queue_as :default
 
   def perform(performer_id, chuds)
-    performer = Performer.lock.find(performer_id)
-    performer.update!(chuds_balance: performer.chuds_balance + chuds)
+    Performer.transaction do
+      performer = Performer.lock.find(performer_id)
+      performer.update!(chuds_balance: performer.chuds_balance + chuds)
+    end
   end
 end
